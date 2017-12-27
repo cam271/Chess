19ec0969c93b1b56774023b4652ba9065372b157
Messenger, C <messenger_c@ymail.com>
2017-12-27 13:40:43 -0500

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Chris on 12/27/17.
+//  Copyright © 2017 Chris Messenger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
